35349573f99442a34c4239c90817283012f0efe0
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-07-04 20:25:13 +0100

    initial file upload, webpack fully setup

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{console.log("hello");for(let l=0;l<10;l++)console.log("hello")})();
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+        ],
+    },
+};
