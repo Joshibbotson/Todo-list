04db7d75e37508639a1c1531a74cf082b11678b8
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-07-08 21:25:17 +0100

    localstorage implemented but not complete, array is pushed to locally and within localstorage

--- src/addtask.js
+++ src/addtask.js
@@ -10,7 +10,33 @@ export function Task (title) {
 }
 
 export function addTasktoArr() {
-    activeArr.push(new Task(addTask.title.value))
+
+    
+    const task = new Task(addTask.title.value)
+    activeArr.push(task)
+    localStorage.setItem('Tasks', JSON.stringify(activeArr))
     console.log(activeArr)
+
+    // let i = //i should be whatever task is returned which correlates an index to modify
+    const todos = JSON.parse(localStorage.getItem('Tasks'))[0].title;
+    console.log(todos)
+
+    createDOMTask()
+}
+
+export function createDOMTask() {
+    // let localStorageTask = JSON.parse(localStorage.getItem('Task'))[0].title
+
+    const main = document.getElementById('main')
+    const div = document.createElement('div')
+    const btn = document.createElement('button')
+    const p = document.createElement('p')
+
+    p.innerHTML = "localStorageTask"
+
+    div.appendChild(p)
+
+    main.appendChild(div)
+
 }
 
